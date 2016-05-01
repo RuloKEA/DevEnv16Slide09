3d2ec8ec916f937181a346a1222536710b7073a8
RuloKea <rulo@kea.dk>
2016-05-01 12:24:15 +0200

    Add project files.

---
+++ DevnEnv16Slide09/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevnEnv16Slide09.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello from home index";
+        }
+
+        public string Hello()
+        {
+            return "Hello from hello ";
+        }
+
+
+        public string Details(int? id )
+        {
+            return "Hello from details "+id;
+        }
+
+        public string HelloYou(string id)
+        {
+            return "Hello from helloyou "+id;
+        }
+    }
+}
---
+++ DevnEnv16Slide09/Controllers/PostsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevnEnv16Slide09.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Create(string name)
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        public string TestAction()
+        {
+            return "String from controller";
+        }
+    }
+}
