0bb26a55644c74fd6cd609dd7657fc5de35dfbd8
RuloKea <rulo@kea.dk>
2016-05-09 10:43:42 +0200

    many changes

--- DevnEnv16Slide09/Controllers/PostsController.cs
+++ DevnEnv16Slide09/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using DevnEnv16Slide09.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,28 +13,108 @@ namespace DevnEnv16Slide09.Controllers
         // GET: Posts
         public ActionResult Index()
         {
-            return View();
+            if (Session["posts"] == null)
+            {
+                List<Post> myPosts = new List<Post>();
+                Post post = new Post {  Title= "junk", Body = "sdfsdf" };
+                Post post1 = new Post { Title = "jwadsaunk", Body = "sdfsasddf"};
+                myPosts.Add(post);
+                myPosts.Add(post1);
+                Session["posts"] = myPosts;
+            }
+            List<Post> posts = (List<Post>)Session["posts"];
+            return View(posts);
         }
-
+        //GET
         public ActionResult About()
         {
             return View();
         }
-
-        public ActionResult Create(string name)
+        //POST
+        [HttpPost]
+        public ActionResult About(string username, string password)
         {
+            
+            return Content(username+" "+password);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if(ModelState.IsValid)
+            {
+                //get list from session
+                List<Post> myPosts = (List<Post>)Session["posts"];
+                // add post to list
+                myPosts.Add(post);
+                //save list in session
+                // Session["posts"] = myPosts;
+                //redrect to index
+                return RedirectToAction("index");
+            }
             return View();
         }
 
         public ActionResult Details(int id)
         {
-            ViewBag.id = id;
+            List<Post> myPosts = (List<Post>)Session["posts"];
+            Post post = myPosts.Find(m => m.PostId == id);
+         
+            return View(post);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            List<Post> myPosts = (List<Post>)Session["posts"];
+            Post post = myPosts.Find(m => m.PostId == id);
+
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                //get list from session
+                List<Post> myPosts = (List<Post>)Session["posts"];
+                Post oldPost = myPosts.Find(p => p.PostId == post.PostId);
+                oldPost.Title = post.Title;
+                oldPost.Body = post.Body;
+
+                //for (int i = 0; i < myPosts.Count; i++)
+                //{
+                //    if (myPosts[i].PostId == post.PostId)
+                //    {
+                //        myPosts[i].Title = post.Title;
+                //        myPosts[i].Body = post.Body;
+                //        break;
+                //    }
+                //}
+
+                //redrect to index
+                return RedirectToAction("index");
+            }
             return View();
         }
 
-        public string TestAction()
+        public ActionResult Delete(int id)
         {
-            return "String from controller";
+            List<Post> myPosts = (List<Post>)Session["posts"];
+            Post post = myPosts.Find(m => m.PostId == id);
+            myPosts.Remove(post);
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Junk()
+        {
+            ViewBag.message = "Hello from controller";
+            return PartialView("_Junk");
         }
     }
 }
---
+++ DevnEnv16Slide09/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DevnEnv16Slide09.Models
+{
+    public class Post
+    {
+        private static int noPosts;
+
+        [Display(Name ="ID")]
+        public int PostId { get; set; }
+
+     
+        [Required ]
+        public string Title { get; set; }
+
+        [Display(Name = "Text")]
+        [Required, StringLength(10, MinimumLength =3)]
+        public string Body { get; set; }
+
+        public Post()
+        {
+            noPosts++;
+            this.PostId = noPosts;
+        }
+            
+       
+    }
+}
